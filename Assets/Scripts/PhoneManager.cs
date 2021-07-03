cc30a5385e365d2d098c085a4b854bd5108bf34a
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2021-07-03 02:33:52 -0700

    Get ios build working.

--- Assets/Scripts/PhoneManager.cs
+++ Assets/Scripts/PhoneManager.cs
@@ -2,37 +2,57 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PhoneManager : MonoBehaviour
 {
+    public string ServerAddress = "192.168.86.47";
+    public int ServerPort = 8080;
     public GyroRotator GyroRotator;
 
     private const int PACKET_SIZE = 17;
     private UdpClient udpClient;
     private byte[] sendBuffer = new byte[PACKET_SIZE];
-    private Thread receiveBroadcastThread;
 
-    private volatile int numBroadcastBytes = 0;
+    private volatile bool isConnectionReady = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        udpClient = new UdpClient();
-        udpClient.EnableBroadcast = true;
-        udpClient.Send(new byte[1] { 1 }, 1, new IPEndPoint(IPAddress.Broadcast, 8080));
+        Task.Run(ConnectToServer);
+    }
 
-        var serverEndpoint = new IPEndPoint(IPAddress.Any, 0);
-        byte[] bytes = udpClient.Receive(ref serverEndpoint);
-        Debug.Log("Broadcast response: " + bytes.Length);
-        udpClient.EnableBroadcast = false;
-        udpClient.Connect(serverEndpoint);
+    private void ConnectToServer()
+    {
+        udpClient = new UdpClient();
+        // Special case on iOS since broadcasting does not work by default.
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            udpClient.Connect(ServerAddress, ServerPort);
+        }
+        else
+        {
+            udpClient.EnableBroadcast = true;
+            udpClient.Send(new byte[1] { 1 }, 1, new IPEndPoint(IPAddress.Broadcast, 8080));
+
+            var serverEndpoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] bytes = udpClient.Receive(ref serverEndpoint);
+            Debug.Log("Broadcast response: " + bytes.Length);
+            udpClient.EnableBroadcast = false;
+            udpClient.Connect(serverEndpoint);
+        }
+        isConnectionReady = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!isConnectionReady)
+        {
+            return;
+        }
+        
         using (var memStream = new MemoryStream(sendBuffer))
         {
             Vector3 rotation = GyroRotator.transform.rotation.eulerAngles;
@@ -49,29 +69,4 @@ public class PhoneManager : MonoBehaviour
     {
         udpClient.Close();
     }
-
-    protected void OnGUI()
-    {
-        GUI.skin.label.fontSize = Screen.width / 40;
-        GUILayout.Label("Num bytes: " + numBroadcastBytes);
-    }
-
-    // private void receiveBroadcastLoop()
-    // {
-    //     while (true)
-    //     {
-    //         ++numBroadcastBytes;
-    //         try
-    //         {
-    //             var hostEndpoint = new IPEndPoint(IPAddress.Any, 0);
-    //             byte[] data = broadcastClient.Receive(ref hostEndpoint);
-    //             numBroadcastBytes += data.Length;
-    //             Debug.Log(string.Format("Data received: {0}", data));
-    //         }
-    //         catch (Exception)
-    //         {
-    //             break;
-    //         }
-    //     }
-    // }
 }
